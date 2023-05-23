972eec4c44d4964fa903e897d4baf32bd7140568
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-05-23 18:37:43 -0300

    feat: adicionando button em rota principal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
-    
+
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Primeira página</p>} />
-            <Route path='*' element={<Navigate to='/pagina-inicial'/>} /> 
+            <Route path="/" element={<Button variant=
+                "contained" color="primary">Testee</Button>} />
+            <Route path='*' element={<Navigate to='/' />} />
         </Routes>
     )
 };
