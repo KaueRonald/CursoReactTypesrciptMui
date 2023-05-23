f2ae17b1154f4961023c6679bd54879c52b05ce0
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-05-23 15:21:24 -0300

    feat: adicionando rotas para página inicial e para qaulquer página inexistente cair na página principal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Primeira página</p>} />
+            <Route path='*' element={<Navigate to='/pagina-inicial'/>} /> 
+        </Routes>
+    )
+};
