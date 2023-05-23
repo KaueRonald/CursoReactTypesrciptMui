994199eb4f3468b7ef5dcb4a96397844bbdf7fb6
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-05-23 15:20:05 -0300

    feat: adicionando react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Menu } from './shared/components/menu'
+import { AppRoutes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Menu/>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/about' element={<App />} />
-      </Routes>
+      <Menu />
+      <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>,
 )
